dab171717fe37375f4ca2d303216d8f467d3684b
igishevandrey433-create <igishevandrey433@gmail.com>
2026-03-18 21:53:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,2 @@
 window.TG_TOKEN = '8639881516:AAF6IT_2kI2XvYf8h8NGhb__QkDie9Ghhpc';
 window.TG_CHAT = '6975821575';
-```
-
-Затем проверьте — откройте в браузере:
-```
-https://api.telegram.org/bot8639881516:AAF6IT_2kI2XvYf8h8NGhb__QkDie9Ghhpc/sendMessage?chat_id=6975821575&text=Тест
