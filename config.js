41134efc03f17a55b1ff637c1b6f66d28797721d
igishevandrey433-create <igishevandrey433@gmail.com>
2026-03-18 21:09:24 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+window.TG_TOKEN = '8639881516:AAF6IT_2kI2XvYf8h8NGhb__QkDie9Ghhpc';
+window.TG_CHAT = '8639881516';
+```
+
+Нажмите **Commit changes**.
+
+---
+
+### Шаг 2 — Добавьте `.gitignore`
+
+Создайте файл **`.gitignore`** и вставьте:
+```
+config.js
